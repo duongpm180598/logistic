80287a98371bee761ff8b75c8f10e2314726269b
Dng <duongpm180598@gmail.com>
2024-09-02 12:48:58 +0700

    Initial commit

--- src/app/shell/shell.component.ts
+++ src/app/shell/shell.component.ts
@@ -1,10 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-shell',
   templateUrl: './shell.component.html',
-  styleUrl: './shell.component.scss'
+  styleUrl: './shell.component.scss',
 })
 export class ShellComponent {
+  windowScrolled: boolean | undefined;
 
+  constructor() {}
+
+  @HostListener('window:scroll', [])
+  onWindowScroll(): void {
+    if (
+      window.pageYOffset ||
+      document.documentElement.scrollTop ||
+      document.body.scrollTop > 250
+    ) {
+      this.windowScrolled = true;
+    } else if (
+      (this.windowScrolled && window.pageYOffset) ||
+      document.documentElement.scrollTop ||
+      document.body.scrollTop < 10
+    ) {
+      this.windowScrolled = false;
+    }
+  }
+
+  scrollToTop(): void {
+    const currentScroll =
+      document.documentElement.scrollTop || document.body.scrollTop;
+    if (currentScroll > 0) {
+      window.scrollTo({ top: 0 });
+    }
+  }
 }
